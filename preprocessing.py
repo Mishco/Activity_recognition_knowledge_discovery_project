f90b85a6c9bcf58fefc0dd95c3cfc9b0a3fc97b7
Bc. Martin Mocko <martin.mocko1@gmail.com>
2017-03-06 15:50:16 +0100

    Uz by malo fungovat robenie sliding window do csv-cka s vela columnami

--- preprocessing.py
+++ preprocessing.py
@@ -14,23 +14,28 @@ def main():
     end = 99
 
     slidedataframe = pd.DataFrame()
-
-
+    iteration=1
+    mycolumns=[]
     while (all_csv.size-1 > end):
         tmp = all_csv.ix[start:end,1:4]
         movement = all_csv.ix[end,4]
         userid = all_csv.ix[end,5]
         tmp = tmp.stack().to_frame().T
-        #tmp.columns = ['{}_{}'.format(*c) for c in tmp.columns]
+
+        if iteration == 1:
+            mycolumns=['{}_{}'.format(*c) for c in tmp.columns]
+            iteration=2
+        tmp.columns = mycolumns
+        # print (tmp.columns)
         tmp['movement'] = movement
         tmp['userid'] = userid
 
         slidedataframe = slidedataframe.append(tmp)
         start += 1
         end += 1
-        if (end > 100):
+        if (end == 100):
         #if (end > 100 == 0):
-            print(end)
+            # print(end)
             print(slidedataframe)
             break
 
