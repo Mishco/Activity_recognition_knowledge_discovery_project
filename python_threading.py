de5f2e1c4fba4b45c566eac4aed55bcff3ab8a58
Michal Slovík <mishcos@gmail.com>
2017-03-07 22:43:05 +0100

    threading update

--- python_threading.py
+++ python_threading.py
@@ -77,17 +77,5 @@ thread2 = SlidingWindow(100,199, "sliding2.csv")
 thread1.start()
 thread2.start()
 
-print(thread1.res)
-print(thread2.res)
-
-# thread1 = SummingThread(0,50000000)
-# thread2 = SummingThread(500000,100000000)
-#
-# thread1.start() # This actually causes the thread to run
-# thread2.start()
-#
-# thread1.join()  # This waits until the thread has completed
-# thread2.join()
-# # At this point, both threads have completed
-# result = thread1.total + thread2.total
-# print (result)
+#print(thread1.res)
+#print(thread2.res)
